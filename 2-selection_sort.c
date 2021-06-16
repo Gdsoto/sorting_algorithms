4ecb86b3a48464f1570e63c215c2100ea21bffb3
Gerson Soto <gersonsoto5622@gmail.com>
2021-06-16 17:01:20 -0500

    Hot fix

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers
+ *
+ * @array: List of integers
+ * @size: Lenght
+ * Return: None
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (!array)
+		return;
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
